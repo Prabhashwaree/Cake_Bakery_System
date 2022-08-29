a57a5158f720f535bab0aca7ce6f31f2013c6cbb
Prabhashearee <prabhashwaree57@gmail.com>
2022-08-29 21:38:31 +0530

    Update first page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,6 +12,9 @@ import SearchIcon from '@mui/icons-material/Search';
 
 import logo from '../../assets/img/logo.PNG';
 import phone from '../../assets/img/Phone.png';
+import mainPic from '../../assets/img/mainBackground.jpg';
+import caketoOrder from '../../assets/img/caketoOrder.PNG';
+import Button from '@mui/material/Button';
 
 
 
@@ -35,6 +38,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 export default function HomePage() {
   return (
+    <>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" >
         <Toolbar className="appBar">
@@ -54,8 +58,10 @@ export default function HomePage() {
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
             // style={{ width: '5px'}}
             >
-            <img src={logo} style={{width:"15%",height:"50px",position:"absolute",top:"10px"}} /></Typography>
-        
+            <img src={logo} style={{width:"15%",height:"50px",position:"absolute",top:"10px"}} />
+            </Typography> 
+            {/* <h6 className="delicious"style={{ position:"absolute",top:"11px",left:"8%",color:"#b44151",cursor:"pointer",fontSize:"34px",fontFamily:"Meow Script, cursive;"}}> Cake Bakery</h6> */}
+
           <a><Typography variant="subtitle1" gutterBottom style={{ position:"absolute",left:"24%",top:"20px",color:"gray",cursor:"pointer",fontSize:"15px"}}> Main</Typography></a>
           <a><Typography variant="subtitle1" gutterBottom style={{ position:"absolute",left:"29%",top:"20px",color:"gray",cursor:"pointer",fontSize:"15px"}}> Catalog</Typography></a>
           <a><Typography variant="subtitle1" gutterBottom style={{ position:"absolute",left:"35.4%",top:"20px",color:"gray",cursor:"pointer",fontSize:"15px"}}> About Us</Typography></a>
@@ -68,5 +74,15 @@ export default function HomePage() {
         </Toolbar>
       </AppBar>
     </Box>
+    <div>
+      <img src={mainPic} style={{width:"100%",height:"88vh"}} />
+      {/* <img src={caketoOrder} style={{width:"30%",height:"20vh",opacity:"0.5",position:"absolute",left:"20px"}} /> */}
+      <h6 className="delicious"style={{ position:"absolute",left:"20px",top:"25%",color:"gray",cursor:"pointer",fontSize:"80px",fontFamily:"Meow Script, cursive;"}}> Delicious Cake</h6>
+      <h6 className="delicious"style={{ position:"absolute",left:"7%",top:"37%",color:"gray",cursor:"pointer",fontSize:"80px",fontFamily:"Meow Script, cursive;"}}> to Order</h6>
+      <Button variant="outlined" style={{ position:"absolute",left:"10%",top:"56%",cursor:"pointer",border:"1px solid #BE6F77",color:"#BE6F77"}}>Coose a Cake</Button>
+    </div>
+
+
+    </>
   );
 }
