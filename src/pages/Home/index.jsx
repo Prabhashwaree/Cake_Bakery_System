60ab558bc5f4a384ee822d84fcdab2bd7c1bfaff
Prabhashearee <prabhashwaree57@gmail.com>
2022-08-30 09:29:47 +0530

    Update secon section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -117,6 +117,22 @@ export default function HomePage() {
       <section className="secondPage">
         <div className="cakeDiscription">
           <div className="cakeDiscriptionRectangle">
+          <h6 className="delicious" style={{ position: "absolute", left: "33%", top: "8%", color: "white", cursor: "pointer", fontSize: "40px", fontFamily: "Meow Script, cursive;" }}> Filling</h6>
+          <p style={{ position: "absolute", left: "20%", top: "25%", color: "white", fontSize: "16.3px"}}>You can choose one of six</p>
+          <p style={{ position: "absolute", left: "25%", top: "31%", color: "white", fontSize: "16.3px"}}>toppings for the cake</p>
+          
+          <div className="discripCatogary">
+               <p className="listSection">Snicker</p>
+               <p className="listSection">Red Velvet</p>
+               <p className="listSection">Oreo cheese</p>
+
+               <p className="listSection">Chery bomb</p>
+               <p className="listSection">Bery</p>
+               <p className="listSection">Bounty</p>
+    
+          </div>
+          
+          <Button variant="outlined" style={{ position: "absolute", left: "18%", top: "83%", cursor: "pointer", border: "1px solid white", color: "white",width:"13vw" }}>To Order</Button>
 
           </div>
         </div>
